843098776f3b1b89a55fb1cbd2505ee62abae9d4
rohinifegade <rohini.fegade01@gmail.com>
2025-05-03 05:39:41 -0700

    Completed Task 2

---
+++ Task2.py
@@ -0,0 +1,10 @@
+# Demonstrate List Slicing 
+
+number_list=[1,2,3,4,5,6,7,8,9,10]
+print("Original list:",number_list)
+
+first_five=number_list[0:5]
+print("First Five Elements:",first_five)
+
+reversed_list=first_five[::-1]
+print("Reversed Extracted Elements:",reversed_list)
